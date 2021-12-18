1efbe53217871bbd775e1ae6eefba46465fef464
fsps60312 <fsps60312@yahoo.com.tw>
2021-12-19 01:33:52 +0800

    browser can navigate url in textbox

--- UI/BrowserPanel.py
+++ UI/BrowserPanel.py
@@ -3,12 +3,17 @@ import wx.html2
 from UI.MyBoxSizer import MyBoxSizer
 
 class BrowserPanel(wx.Panel):
+    '''https://wxpython.org/Phoenix/docs/html/wx.html2.WebView.html#wx-html2-webview'''
+
     def __init__(self, *args, **kwargs):
         wx.Panel.__init__(self, *args, **kwargs)
         self.__init_UI()
 
     def __init_UI(self):
-        self.webview = wx.html2.WebView.New(self, url='http://codingsimplifylife.blogspot.tw/')
+        self.webview: wx.html2.WebView = wx.html2.WebView.New(self, url='http://codingsimplifylife.blogspot.tw/')
         self.sizer = MyBoxSizer(self, orient=wx.VERTICAL, addmany_list=[
             (self.webview, wx.SizerFlags(1).Expand())
         ])
+    
+    def LoadURL(self, url: str):
+        self.webview.LoadURL(url)
--- UI/MainPanel.py
+++ UI/MainPanel.py
@@ -6,16 +6,25 @@ class MainPanel(wx.Panel):
     def __init__(self, *args, **kwargs):
         wx.Panel.__init__(self, *args, **kwargs)
         self.__init_UI()
+        self.__init_events()
+
+    def __init_events(self):
+        self.button.Bind(wx.EVT_BUTTON, lambda e: [
+            w:=self.text_entry.GetValue(),
+            self.TopLevelParent.SetTitle(w),
+            self.browser_panel.LoadURL(w)])
     
     def __init_UI(self):
         self.control_panel = wx.Panel(self)
 
-        self.browser_panel = BrowserPanel(self)
         self.text_entry = wx.TextCtrl(self)
+        self.button = wx.Button(self, label='I\'m button')
+
+        self.browser_panel = BrowserPanel(self)
 
         self.control_panel.sizer = MyBoxSizer(self.control_panel, orient=wx.HORIZONTAL, addmany_list=[
             (self.text_entry, wx.SizerFlags(1).Expand()),
-            (wx.Button(self, label='I\'m button'), wx.SizerFlags(0).Expand())
+            (self.button, wx.SizerFlags(0).Expand())
         ])
         
         self.sizer = MyBoxSizer(self, orient=wx.VERTICAL, addmany_list=[
