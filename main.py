584b3240e2247dd1204a2f4cf3e4dde4c46718f4
fsps60312 <fsps60312@yahoo.com.tw>
2021-12-24 02:39:19 +0800

    add icon

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from UI.MainPanel import MainPanel
 app = wx.App()
 
 frame = wx.Frame(None, title='RealTimeTranslate4', size=(1000, 500))
+frame.SetIcon(wx.Icon('main.ico'))
 main_panel = MainPanel(frame)
 frame.Show()
 
